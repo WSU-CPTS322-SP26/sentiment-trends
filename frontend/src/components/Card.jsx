9346283ff9160dd5398e6de2cf77e04c0b7c0b69
Troy <troymuehlbauer1@gmail.com>
2026-02-21 22:57:39 -0800

    single card object prop

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,22 +1,22 @@
 import styles from "../styles/components/Card.module.css";
 import Bar from "./Bar";
 
-const Card = ({title, image, negative_sentiment, neutral_sentiment, positive_sentiment, compound_sentiment}) => {
+const Card = ({card}) => {
     return (
         <div className={styles.card}>
             <div className={styles.cardHeader}>
                 <img className={styles.cardImage}
-                src={image}
-                alt={title}>
+                src={card.image}
+                alt={card.title}>
                 </img>
-                <h2 className={styles.cardTitle}>{title}</h2>
+                <h2 className={styles.cardTitle}>{card.title}</h2>
             </div>
             <div className={styles.cardBody}>
-                <p>Compound Sentiment: {compound_sentiment}</p>
-                <Bar negative={negative_sentiment} 
-                    neutral={neutral_sentiment} 
-                    positive={positive_sentiment} 
-                    compound={compound_sentiment}
+                <p>Compound Sentiment: {card.compound_sentiment}</p>
+                <Bar negative={card.negative_sentiment} 
+                    neutral={card.neutral_sentiment} 
+                    positive={card.positive_sentiment} 
+                    compound={card.compound_sentiment}
                 />
             </div>
         </div>
