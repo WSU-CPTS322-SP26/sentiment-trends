f1cf0ed520ae77988b2b68c7dee068a05bb964dc
CAESARSWRLD <166182428+CAESARSWRLD@users.noreply.github.com>
2026-03-21 03:56:22 -0700

    make sure track_topics still runs in actions

--- backend/services/track_topics.py
+++ backend/services/track_topics.py
@@ -2,34 +2,76 @@ import os
 from dotenv import load_dotenv
 from supabase import create_client
 from datetime import datetime, date
+from nltk.sentiment import SentimentIntensityAnalyzer
+from atproto import Client
+import uuid
+
+#First)   get trending topics
+#Second)  load 20 posts for each trending topic
+#Third)   run sentiment analysis on each post
+#Fourth)  store sentiment data and topic in supabase db
+
+
+
 
 load_dotenv()
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_SERVICE_KEY")
-
-supabase = create_client(url, key)
-
-#test query
-response = supabase.table("users").select("*").limit(1).execute()
 
 
-print("connection successful")
-print(response.data)
+bluesky_handle = os.getenv("BLUESKY_HANDLE")
+bluesky_password = os.getenv("BLUESKY_APP_PASSWORD")
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_SERVICE_KEY")
+
+client = Client()
+client.login(bluesky_handle, bluesky_password)
+
+
+sia = SentimentIntensityAnalyzer()
+
+
+
+results = client.app.bsky.feed.search_posts(
+    {
+        "q": "Ford Motor Company",
+        "limit": 10
+    }
+)
+
+for post in results.posts:
+    print(post.record.text)
+    print(sia.polarity_scores(post.record.text))
+for post in results.posts:
+    print(post.record.text)
+
+
+
+
+
+
+
+
+supabase = create_client(supabase_url, supabase_key)
+
+
+
+
 
 
 positive = 0
 negative = 0
 neutral = 0
 
+row_id = str(uuid.uuid4())
 
 today = date.today().isoformat()
-supabase.table("table").upsert(
+supabase.table("data").upsert(
     {
-        "id": today,
-        "positive_sentiment": positive,
-        "negative_sentiment": negative,
-        "neutral_sentiment": neutral
+        "id": row_id,
+        "pos": positive,
+        "neu": neutral,
+        "neg": negative,
+        "topic": "the topic!"
     },
     on_conflict="id"
 ).execute()
